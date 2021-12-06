c87cdf534d0c114b1182682055a9686e97211328
Samuel Martins <francisco.martins@vaivoa.com>
2021-12-06 15:26:15 -0300

    feature collection view

---
+++ desafio-modal/src/controllers/GitCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GitCollectionViewCell.swift
+//  desafio-modal
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 06/12/21.
+//
+
+import UIKit
+
+class GitCollectionViewCell: UICollectionViewCell {
+    static let identifier = "GitCollectionViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
