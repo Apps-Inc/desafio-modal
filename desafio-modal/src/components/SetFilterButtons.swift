6e57289fdf6727c4a258a5d3972e0f0495e009dc
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2021-12-07 17:40:43 -0300

    adequação dos cliques para permitir retirar a ordem totalmente

--- desafio-modal/src/components/SetFilterButtons.swift
+++ desafio-modal/src/components/SetFilterButtons.swift
@@ -7,44 +7,40 @@
 
 import UIKit
 
-class SetFilterButton: UIView {
+class SetFilterButtons: UIView {
     var view: UIView!
-    var filtros = ["CRESCENTE"]
+    var filtros = [String]()
     @IBOutlet var decrescente: UIButton!
     @IBOutlet var crescente: UIButton!
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         xibSetup()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         xibSetup()
-        crescente.isSelected = true
-        formatButtonSelected(button: crescente)
     }
-    
+
     func xibSetup() {
         view = loadViewFromNib()
         view.frame = bounds
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    
         addSubview(view)
     }
     func loadViewFromNib() -> UIView {
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: "SetFilterButtons", bundle: bundle)
-        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
-    
+        let view = nib.instantiate(withOwner: self, options: nil).first as? UIView
+        guard let view = view else { return self.view }
         return view
     }
-    
-  
+
     @IBAction func filterButtons(_ sender: Any) {
         guard let button = sender as? UIButton else { return }
         button.isSelected = button.isSelected ? false : true
-        
+
         if button.isSelected {
             filtros.append(button.currentTitle!)
             formatButtonSelected(button: button)
@@ -55,13 +51,27 @@ class SetFilterButton: UIView {
         }
         print(filtros)
     }
-    
-    
+
     @IBAction func orderFilter(_ sender: Any) {
         guard let button = sender as? UIButton else { return }
-        if button.isSelected { return }
+
+        if !decrescente.isSelected && !crescente.isSelected {
+            filtros.append(button.currentTitle!)
+            formatButtonSelected(button: button)
+            button.isSelected = true
+            print("teste")
+            return
+        }
+
+        if button.isSelected {
+            let index = filtros.firstIndex(of: button.currentTitle!)
+            filtros.remove(at: index!)
+            formatButtonUnSelected(button: button)
+            button.isSelected = false
+            return
+        }
         button.isSelected = true
-        
+
         if button.currentTitle! == "CRESCENTE" {
             filtros.append(button.currentTitle!)
             let index = filtros.firstIndex(of: "DECRESCENTE")
@@ -79,18 +89,18 @@ class SetFilterButton: UIView {
         }
         print(filtros)
     }
-    
+
     func formatButtonSelected(button: UIButton) {
         button.backgroundColor = .black
         button.layer.cornerRadius = 5
         button.setTitleColor(.white, for: .selected)
-        button.setImage(UIImage(systemName: "checkmark"), for:.selected)
+        button.setImage(UIImage(systemName: "checkmark"), for: .selected)
     }
-    
+
     func formatButtonUnSelected(button: UIButton) {
         button.backgroundColor = .white
         button.titleLabel?.textColor = .black
         button.setImage(UIImage(named: ""), for: .normal)
     }
-    
+
 }
