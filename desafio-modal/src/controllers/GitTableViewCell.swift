9ef2f51d1630c2bf2f869fd903ebfdb8e724e90f
Samuel Martins <francisco.martins@vaivoa.com>
2021-12-09 17:15:14 -0300

    adding tableview cell outlets

--- desafio-modal/src/controllers/GitTableViewCell.swift
+++ desafio-modal/src/controllers/GitTableViewCell.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class GitTableViewCell: UITableViewCell {
     static let identifier = "GitTableViewCell"
-
+    @IBOutlet weak var repositoryLabel: UILabel!
+    @IBOutlet weak var gitImage: UIImageView!
+    @IBOutlet weak var starCountLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var labelExtra: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
