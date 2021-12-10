f0c8335f493d5d7742cdebfd83d41f60dd13325f
diogoneiss <diogo.oliveiran@gmail.com>
2021-12-09 23:47:57 -0300

    xib basic para search, necessário ver como estilizar na viewmodel devida

---
+++ desafio-modal/src/components/SearchInputButton.swift
@@ -0,0 +1,55 @@
+//
+//  SearchInputButton.swift
+//  desafio-modal
+//
+//  Created by DIOGO OLIVEIRA NEISS on 09/12/21.
+//
+
+import UIKit
+
+class SearchInputButton: UIView {
+
+   //TODO: tirar o text field background, mover o texto pra esquerda, consertar bordas arredondadas, aumentar tamanho do text field
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    
+    var view: UIView!
+    
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "SearchInputButton", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as? UIView
+        guard let view = view else { return self.view }
+        return view
+    }
+
+}
