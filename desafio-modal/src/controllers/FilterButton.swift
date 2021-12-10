e79c87b7a3eb75d69c81d75f802aea07170c55f9
Aníbal Vilela <anibal.vilela@vaivoa.com>
2021-12-10 14:39:12 -0300

    Localização

--- desafio-modal/src/controllers/FilterButton.swift
+++ desafio-modal/src/controllers/FilterButton.swift
@@ -1,13 +1,33 @@
 import Foundation
 
-enum FilterButton: String, CaseIterable, Hashable {
-
-    case star = "Estrelas"
-    case followers = "Seguidores"
-    case date = "Data"
-    case order = "ordem"
+enum FilterButton: CaseIterable, Hashable {
+    case star
+    case followers
+    case date
+    case order
 
     var name: String {
-        return self.rawValue
+        let languageCode = Locale.preferredLanguages.first ?? "pt-BR"
+        return self.localizedName(language: languageCode)!
+    }
+
+    private func localizedName(language code: String) -> String? {
+        localizations[code]?[self]
     }
 }
+
+private let localizations: [String: [FilterButton: String]] = [
+    "pt-BR": [
+        .star: "Estrelas",
+        .followers: "Seguidores",
+        .date: "Data",
+        .order: "ordem"
+    ],
+
+    "en": [
+        .star: "Stars",
+        .followers: "Followers",
+        .date: "Date",
+        .order: "order"
+    ]
+]
